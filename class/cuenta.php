b9d8e2977fa3cc6bd3ae6db13f56dd9835fd5818
CamiRinconR <maluiso9855@gmail.com>
2024-03-08 20:54:48 -0500

    Prueba de commit

--- class/cuenta.php
+++ class/cuenta.php
@@ -1,6 +1,21 @@
 <?php
 class Cuenta 
 {
+    #atributos
+    private $nombre;
+    private $numeroCuenta;
+    private $tipoInteres;
+    private $saldo;
+
+    #atributos de clase
+    public function __construct($nombre, $numeroCuenta, $tipoInteres, $saldo)
+    {
+        $this->nombre = $nombre;
+        $this->numeroCuenta = $numeroCuenta;
+        $this->tipoInteres = $tipoInteres;
+        $this->saldo = $saldo;
+    }
+
     
 };
 
